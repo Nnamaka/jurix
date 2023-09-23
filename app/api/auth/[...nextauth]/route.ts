381d194f3f11171a11857a6b1ba37835a0cc5e4d
Nnamaka <nnamaka7@gmail.com>
2023-09-23 16:29:35 +0100

    music

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,21 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+// import NextAuth , { NextAuthOptions  }from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
-    }),
-  ],
-};
+// const authOptions: NextAuthOptions  = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID! as string,
+//       clientSecret: process.env.GOOGLE_SECRET! as string,
+//     }),
+//   ],
+// };
 
-const handler = NextAuth(authOptions);
+// const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST, authOptions };
+import NextAuth from "next-auth/next"
+import { authOptions } from "@/utils/authOptions";
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST, authOptions }
